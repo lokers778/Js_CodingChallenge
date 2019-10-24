0be425c18afbd49a18f43d8bdb22849a0987d0e9
Łukasz <lokers778@gmail.com>
2019-10-24 23:06:50 +0200

    finished day 6

--- subpages/Js6_Ajax_Type_Ahead/Js6 Ajax type Ahead.js
+++ subpages/Js6_Ajax_Type_Ahead/Js6 Ajax type Ahead.js
@@ -21,9 +21,21 @@ const start=(wordToMatch,cities)=>{
 function displayMatches(){
    const matchArray= start(this.value,cities)
     console.log(matchArray)
+    const html=matchArray.map(place=>{
+        const regex=new RegExp(this.value, "gi");
+        const cityName=place.city.replace(regex, `<span class="h1">${this.value}</span>`)
+        const stateName=place.state.replace(regex, `<span class="h1">${this.value}</span>`)
+        return `
+        <li>
+        <span class="name">${cityName}, ${stateName}</span>
+        <span class="population">${place.population}</span>
+        </li>
+        `
+    }).join("")
+    suggestions.innerHTML=html
 }
 
-
+const form=document.querySelector("form")
 const searchInput =document.querySelector(".search")
 const suggestions =document.querySelector(".suggestions")
 searchInput.addEventListener("change",displayMatches)
